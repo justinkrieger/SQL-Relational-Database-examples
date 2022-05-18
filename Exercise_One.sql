c75ee75e239c4eff2d72689bacd7f98e499170f9
Justin Krieger <100953590+justinkrieger@users.noreply.github.com>
2022-05-18 12:00:08 -0700

    Update and rename exercises_one.sql to Exercise_One.sql

--- Exercise_One.sql
+++ Exercise_One.sql
@@ -3,6 +3,7 @@ SELECT *
 FROM Vendors JOIN Invoices
 ON Vendors.VendorID = Invoices.VendorID;
 
+
 -- Problem 2
 SELECT VendorName, InvoiceNumber, InvoiceDate,
     InvoiceTotal - (PaymentTotal + CreditTotal) as Balance
@@ -11,10 +12,12 @@ FROM Vendors JOIN Invoices
 WHERE (InvoiceTotal - (PaymentTotal + CreditTotal)) > 0
 ORDER BY VendorName;
 
+
 -- Problem 3
 SELECT VendorName, DefaultAccountNo, AccountDescription
 FROM Vendors JOIN GLAccounts
     ON Vendors.DefaultAccountNo = GLAccounts.AccountNo;
+    
     
 -- Problem 4
 SELECT VendorName, InvoiceNumber, InvoiceDate,
@@ -23,6 +26,7 @@ FROM Vendors, Invoices
 WHERE Vendors.VendorID = Invoices.VendorID AND
     (InvoiceTotal - (PaymentTotal + CreditTotal)) > 0
 ORDER BY VendorName;
+
 
 -- Problem 5
 SELECT VendoName AS Vendor, InvoiceDate AS Date, InvoiceNumber AS Number,
@@ -33,6 +37,7 @@ FROM Vendors v JOIN Invoices i
     ON li.InvoiceID = i.InvoiceID
 ORDER BY Vendor, Date, Number, (#);
 
+
 -- Problem 6
 SELECT DISTINCT v1.VendorID, v1.VendorName,
     v1.VendorContactFName + ' ' + v1.VendorContactLName as Name
@@ -41,12 +46,14 @@ FROM Vendors as v1 JOIN Vendors as v2
         (v1.VendorContactFName = v2.VendorContactFName)
 ORDER BY Name;
 
+
 -- Problem 7
 SELECT GLAccounts.AcountNo, AccountDescription
 FROM GLAccounts LEFT JOIN InvoiceLineItems
     ON GLAccounts.AccountNo = InvoiceLineItems.AccountNo
 WHERE InvoiceLineItems.AccountNo IS NULL
 ORDER BY GLAccounts.AccountNo;
+
 
 -- Problem 8
     SELECT VendorName, VendorState
@@ -58,11 +65,13 @@ UNION
     WHERE VendorState <> 'CA'
 ORDER BY VendorName;
 
+
 -- Problem 9
 SELECT CategoryName, ProductName, ListPrice
 FROM Categories JOIN Products
     ON Categories.CategoryID = Products.CategoryID
 ORDER BY CategoryName, ProductName ASC;
+
 
 -- Problem 10
 SELECT FirstName, LastName, Line1, City, State, ZipCode
@@ -70,12 +79,14 @@ FROM Customers JOIN Addresses
     ON Customers.CustomerID = Addresses.CustomerID
 WHERE Customers.EmailAddress = 'allan.sherwood@yahoo.com';
 
+
 -- Problem 11
 SELECT FirstName, LastName, Line1, City, State, Zipcode
 FROM Customers JOIN Addresses
     Customers.CustomerID = Addresses.CustomerID AND
         Customers.ShippingAddressID = Addresses.AddressID;
-        
+       
+       
 -- Problem 12
 SELECT c.LastName, c.FirstName, OrderDate, p.ProductName, ItemPrice, DiscounAount, Quantity
 FROM Customers c
@@ -84,16 +95,19 @@ FROM Customers c
     JOIN Products p ON oi.ProductID = p.ProductID
 ORDER BY LastName, OrderDate, ProductName;
 
+
 -- Problem 13
 SELECT p1.ProductID, p1.ProductName, p1.ListPrice
 FROM Products as p1 JOIN Products as p2
     ON p1.ProductID <> p2.ProductID AND p1.ListPrice = p2.ListPrice;
+    
     
 -- Problem 14
 SELECT CategoryName, ProductID
 FROM Categories LEFT JOIN PRoducts
     ON Categories.CateogryID = Products.CategoryID
     WHERE ProductID IS NULL;
+    
     
 -- Problem 15
     SELECT 'SHIPPPED' as ShipStatus, OrderID, OrderDate
