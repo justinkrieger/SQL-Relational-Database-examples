3d838981035d0bec7f7c6d4f3974eb15fa5aed79
Justin Krieger <100953590+justinkrieger@users.noreply.github.com>
2022-05-18 13:43:30 -0700

    Update Exercise_Two.sql

--- Exercise_Two.sql
+++ Exercise_Two.sql
@@ -1,6 +1,7 @@
 -- Problem 1
 SELECT CourseNumber, CourseDescription, DepartmentName
-FROM Courses JOIN Departments
+FROM Courses
+JOIN Departments
     ON Courses.DepartmentID = Departments.DepartmentID
 ORDER BY DepartmentName, CourseNumber ASC;
 
@@ -19,22 +20,22 @@ ORDER BY LastName, FirstName;
 -- Problem 3
 SELECT DepartmentName, CourseDescription, FirstName, LastName
 FROM Departments d
-    JOIN Course c
-        ON d.DepartmentID = c.DepartmentID
-    JOIN Instructors i
-        ON d.DepartmentID = i.DepartmentID
+JOIN Course c
+    ON d.DepartmentID = c.DepartmentID
+JOIN Instructors i
+    ON d.DepartmentID = i.DepartmentID
 ORDER BY DepartmentName, CourseDescription ASC;
 
 
 -- Problem 4
 SELECT DepartmentName, CourseDescription, FirstName, LastName
 FROM Departments
-    JOIN Courses
-        ON Departments.DepartmentID = Courses.DepartmentID
-    JOIN StudentCourses
-        ON Courses.CourseID = StudentCourses.CourseID
-    JOIN Students
-        ON StudentCourses.StudentID = Students.StudentID
+JOIN Courses
+    ON Departments.DepartmentID = Courses.DepartmentID
+JOIN StudentCourses
+    ON Courses.CourseID = StudentCourses.CourseID
+JOIN Students
+    ON StudentCourses.StudentID = Students.StudentID
 WHERE DepartmentName = 'English'
 ORDER BY DepartmentName, CourseDescription ASC;
 
@@ -42,8 +43,8 @@ ORDER BY DepartmentName, CourseDescription ASC;
 -- Problem 5
 LastName, FirstName, CourseDescription
 FROM Instructors
-    LEFT JOIN Courses
-        ON Instructors.InstructorID = Courses.InstructorID
+LEFT JOIN Courses
+    ON Instructors.InstructorID = Courses.InstructorID
 ORDER BY LastName, FirstName;
 
 
@@ -63,8 +64,8 @@ ORDER BY EnrollmentDate;
 -- Problem 7
 SELECT DepartmentName, CourseID
 FROM Departments
-    FULL OUTER JOIN Courses
-        ON Departments.DepartmentID = Courses.DepartmentID
+FULL OUTER JOIN Courses
+    ON Departments.DepartmentID = Courses.DepartmentID
 WHERE CourseID IS NULL;
 
 
@@ -73,10 +74,10 @@ SELECT d2.DepartmentName AS InstructorDept,
     FirstName, LastName, CourseDescription,
     d1.DepartmentName as CourseDept
 FROM Courses
-    JOIN Departments d1
-        ON Courses.DepartmentID = d1.DepartmentID
-    JOIN Instructors
-        ON Course.InstructorID = Instructors.InstructorID
-    JOIN Departments d1
-        ON Instructors.DepartmentID = d2.DepartmentID
+JOIN Departments d1
+    ON Courses.DepartmentID = d1.DepartmentID
+JOIN Instructors
+    ON Course.InstructorID = Instructors.InstructorID
+JOIN Departments d1
+    ON Instructors.DepartmentID = d2.DepartmentID
 WHERE d1.DepartmentID <> d2.DepartmentName;
